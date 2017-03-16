1c0a84f040bc6bd250406e6baa04abee16a32413
Evangelos Chatzikalymnios <chatzikalymnios@gmail.com>
2017-03-16 14:14:15 +0100

    ByteBuffer serialization

--- src/main/java/ibis/constellation/impl/ActivityWrapper.java
+++ src/main/java/ibis/constellation/impl/ActivityWrapper.java
@@ -6,11 +6,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ibis.constellation.Activity;
+import ibis.constellation.ByteBufferCache;
 import ibis.constellation.ByteBuffers;
 
 public class ActivityWrapper {
@@ -51,7 +54,25 @@ public class ActivityWrapper {
 			oos.writeObject(activity);
 			
 			if (activity != null && activity instanceof ByteBuffers) {
-				
+				ArrayList<ByteBuffer> list = new ArrayList<ByteBuffer>();
+				((ByteBuffers) activity).pushByteBuffers(list);
+				if (logger.isDebugEnabled()) {
+                    logger.debug("Writing " + list.size() + " bytebuffers");
+                }
+                
+				oos.writeInt(list.size());
+                for (ByteBuffer b : list) {
+                    b.position(0);
+                    b.limit(b.capacity());
+                    oos.writeInt(b.capacity());
+                }
+                
+                for (ByteBuffer b : list) {
+                    oos.write(b.array());
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Wrote bytebuffer of size " + b.capacity());
+                    }
+                }
 			}
 			
 			oos.close();
@@ -71,6 +92,34 @@ public class ActivityWrapper {
 		try {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
 			activity = (Activity) ois.readObject();
+			
+			if (activity != null && activity instanceof ByteBuffers) {
+                int nByteBuffers = ois.readInt();
+                ArrayList<ByteBuffer> list = new ArrayList<ByteBuffer>();
+                
+                if (nByteBuffers > 0) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Reading " + nByteBuffers + " bytebuffers");
+                    }
+                    
+                    for (int i = 0; i < nByteBuffers; i++) {
+                        int capacity = ois.readInt();
+                        ByteBuffer b = ByteBufferCache.getByteBuffer(capacity, false);
+                        list.add(b);
+                    }
+                    
+                    for (ByteBuffer b : list) {
+                        b.position(0);
+                        b.limit(b.capacity());
+                        byte[] buffer = new byte[b.capacity()];
+                        ois.read(buffer, 0, b.capacity());
+                        b.put(buffer, 0, b.capacity());
+                    }
+                }
+                
+                ((ByteBuffers) activity).popByteBuffers(list);
+            }
+			
 			ois.close();
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
